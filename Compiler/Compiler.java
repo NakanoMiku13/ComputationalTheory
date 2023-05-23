c445d31bcc1066f292c13dcf278cf064de541d18
NakanoMiku13 <danielulrichtamayo@gmail.com>
2023-05-22 21:17:41 -0600

    Compiler first commit

---
+++ Compiler/Compiler.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+ class Pair<T1,T2>{
+	public T1 first;
+	public T2 second;
+	public Pair(T1 first, T2 second){ this.first = first; this.second = second; }
+}
+public class Compiler{
+	private static Map<String,String> dicts;
+	private static Pair<String,String> GetKeyAndValue;
+	public static void main(String[] args){
+		dicts = new HashMap<String, String>();
+		System.out.println("Hello");
+		String dictionariesPath = "Dictionaries/";
+		File[] files = new File(dictionariesPath).listFiles();
+		for(File file : files){
+			try{
+				FileReader reader = new FileReader(file);
+				BufferedReader buffer = new BufferedReader(reader);
+				String line;
+				while((line = buffer.readLine()) != null){
+					System.out.println(line);
+				}
+			}catch(Exception ex){}
+		}
+	}
+}
